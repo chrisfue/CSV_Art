dd35938b65eeba38adfc1745434206599fba6aa5
chrisfue <c.fuerst1@gmx.at>
2023-10-26 14:02:51 +0200

    filemanager and KivyMD added

--- main.py
+++ main.py
@@ -1,10 +1,50 @@
-import kivy
-from kivy.app import App
-from kivy.uix.label import Label
+#import kivy
+from kivymd.app import MDApp
+from kivymd.uix.filemanager import MDFileManager
+from kivymd.uix.floatlayout import FloatLayout
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+import os
 
-class MyApp(App):
+class LayoutVars(FloatLayout):
+                 label_text = StringProperty("tbd")
+    
+
+class CSV_Art(MDApp):
+
+    fileManager = None
+    #label_text = StringProperty("to be defined")
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.path = os.path.expanduser("~") or os.path.expanduser("/")
+
+        self.fileManager = MDFileManager(
+            select_path= self.select_path,
+            exit_manager=self.close_fileManager,
+            ext=['.csv']
+            
+        )
+
+    def open_filemanager(self):
+        self.fileManager.show(self.path)
+        
+
+    def select_path(self,path: str):
+        self.close_fileManager()
+        self.root.ids.selected_file.text = f"{path}"
+        
+
+    def close_fileManager(self,*args):
+        self.fileManager.close()
+        
+    
     def build(self):
-        return Label(text="CSV_app_test")
+         return Builder.load_file("app.kv")
+
+
     
 if __name__ == "__main__":
-    MyApp().run()
+    app = CSV_Art()
+    app.run()
